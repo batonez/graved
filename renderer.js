a0438c60fbe39ead3bbb0d67e09125937e4ca0b0
batonez <batonez@gmail.com>
2018-06-05 13:52:25 +0300

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,293 @@
+/*
+Copyright 2018 Anton Jebrak
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software
+without restriction, including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+function drawArrow(ctx, fromx, fromy, tox, toy)
+{
+  //variables to be used when creating the arrow
+  var headlen = 10;
+  ctx.lineWidth = 2;
+
+  var angle = Math.atan2(toy-fromy,tox-fromx);
+
+  //starting path of the arrow from the start square to the end square and drawing the stroke
+  ctx.beginPath();
+  ctx.moveTo(fromx, fromy);
+  ctx.lineTo(tox, toy);
+  ctx.stroke();
+
+  //starting a new path from the head of the arrow to one of the sides of the point
+  ctx.beginPath();
+  ctx.moveTo(tox, toy);
+  ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),toy-headlen*Math.sin(angle-Math.PI/7));
+
+  //path from the side point of the arrow, to the other side point
+  ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/7),toy-headlen*Math.sin(angle+Math.PI/7));
+
+  //path from the side point back to the tip of the arrow, and then again to the opposite side point
+  ctx.lineTo(tox, toy);
+  ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),toy-headlen*Math.sin(angle-Math.PI/7));
+
+  //draws the paths created above
+  ctx.stroke();
+  ctx.fill();
+}
+
+function drawRect(ctx, rect, drawRectId = undefined, translation = {x: 0, y: 0})
+{
+  if (rect["selected"] == 1) {
+    ctx.fillStyle   = "#22FF55";
+  } else if (rect["selected"] == 2) {
+    ctx.fillStyle   = "#229955";
+  } else {
+    ctx.fillStyle   = "#66391A";
+  }
+  
+  ctx.strokeStyle = "#66391A";
+
+  ctx.fillRect(rect.x - rect.width / 2 + translation.x,
+    rect.y - rect.height / 2 + translation.y,
+    rect.width,
+    rect.height
+  );
+
+  ctx.fillStyle = "#FFFFFF";
+  ctx.strokeStyle = "#000000"
+  ctx.font = "14pt Arial";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  ctx.fillText(drawRectId == undefined ? rect.value : drawRectId, rect.x + translation.x, rect.y + translation.y);
+}
+
+function drawRectName(ctx, rect, translation = {x: 0, y: 0})
+{
+  ctx.fillStyle = "#FF0000";
+  ctx.strokeStyle = "#F00";
+  ctx.font = "10pt serif";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "bottom";
+  ctx.fillText(rect.text, rect.x + translation.x, rect.y - rect.height/2 + translation.y);
+}
+
+function lineIntersectionBy4Points(
+  line1point1x, line1point1y, line1point2x, line1point2y,
+  line2point1x, line2point1y, line2point2x, line2point2y)
+{
+  var epsilon = 0.0001;
+  var resultX, resultY;
+  // y=kx+b:
+  // y = ((y2-y1)/(x2-x1))*x + (x2*y1 - x1*y2)/(x2-x1)
+
+  var k1 = (line1point2y - line1point1y)/(line1point2x - line1point1x);
+  var k2 = (line2point2y - line2point1y)/(line2point2x - line2point1x);
+  var b1 = (line1point2x*line1point1y - line1point1x*line1point2y)/(line1point2x - line1point1x);
+  var b2 = (line2point2x*line2point1y - line2point1x*line2point2y)/(line2point2x - line2point1x);
+
+  if (Math.abs(line1point1x - line1point2x) < epsilon) {
+    //console.log("Line 1 is vertical");
+    if (Math.abs(line2point1x - line2point2x) < epsilon) {
+      //console.log("Line 2 is vertical");
+      return undefined;
+    }
+
+    resultX = line1point1x;
+    resultY = k2 * resultX + b2;
+    return {x: resultX, y: resultY};
+  } else if (Math.abs(line2point1x - line2point2x) < epsilon) {
+    resultX = line2point1x;
+    resultY = k1 * resultX + b1;
+    return {x: resultX, y: resultY};
+  }
+
+  if (Math.abs(k1 - k2) < epsilon) {
+    return undefined; // lines are parallel
+  }
+
+  // k1*x + b1 = k2 * x + b2
+  resultX = (b2 - b1)/(k1 - k2);
+  resultY = k1 * resultX + b1;
+
+  return {x: resultX, y: resultY};
+}
+
+function segmentIntersectionBy4Points(
+  seg1point1x, seg1point1y, seg1point2x, seg1point2y,
+  seg2point1x, seg2point1y, seg2point2x, seg2point2y)
+{
+  //console.log("Rect segment from: " + seg1point1x, seg1point1y);
+  //console.log("Rect segment to: " + seg1point2x, seg1point2y);
+
+  var lineIntersection = lineIntersectionBy4Points(
+    seg1point1x, seg1point1y, seg1point2x, seg1point2y,
+    seg2point1x, seg2point1y, seg2point2x, seg2point2y);
+
+  if (lineIntersection == undefined)
+  {
+    //console.log("Lines are parallel");
+    return undefined;
+  }
+
+  //console.log("Found line intersection: " + lineIntersection.x + "; " + lineIntersection.y);
+
+  if (lineIntersection.x < Math.min(seg1point1x, seg1point2x))
+    return undefined;
+
+  if (lineIntersection.x < Math.min(seg2point1x, seg2point2x))
+    return undefined;
+
+  if (lineIntersection.x > Math.max(seg1point1x, seg1point2x))
+    return undefined;
+
+  if (lineIntersection.x > Math.max(seg2point1x, seg2point2x))
+    return undefined;
+
+
+  if (lineIntersection.y < Math.min(seg1point1y, seg1point2y))
+    return undefined;
+
+  if (lineIntersection.y < Math.min(seg2point1y, seg2point2y))
+    return undefined;
+
+  if (lineIntersection.y > Math.max(seg1point1y, seg1point2y))
+    return undefined;
+
+  if (lineIntersection.y > Math.max(seg2point1y, seg2point2y))
+    return undefined;
+
+  //console.log("... And it's inside segments");
+  return lineIntersection;
+}
+
+function drawWeightCircleBetweenRects(ctx, first_rect, second_rect, weight, translation = {x: 0, y: 0})
+{
+  var arrowCenterX = Math.abs(first_rect.x - second_rect.x) / 2 + Math.min(first_rect.x, second_rect.x);
+  var arrowCenterY = Math.abs(first_rect.y - second_rect.y) / 2 + Math.min(first_rect.y, second_rect.y);
+  
+  ctx.fillStyle = "#EEEEEE";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.arc(arrowCenterX + translation.x, arrowCenterY + translation.y, 20, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+  
+  ctx.fillStyle = "#444444";
+  ctx.strokeStyle = "#000000"
+  ctx.font = "10pt Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(Number.parseFloat(weight).toFixed(3).toString(), arrowCenterX + translation.x, arrowCenterY + translation.y);
+}
+
+function drawArrowBetweenRects(ctx, first_rect, second_rect, weight = 0, draw_weights, translation = {x: 0, y: 0})
+{
+  if (first_rect["selected"] != undefined && first_rect["selected"] != 0) {
+    ctx.fillStyle   = "#229955";
+    ctx.strokeStyle   = "#229955";
+  } else {
+    ctx.fillStyle   = "#66391A";
+    ctx.strokeStyle = "#66391A";
+  }
+  
+  var firstPoint, secondPoint, thirdPoint, fourthPoint;
+
+  //console.log("Arrow line from: " + first_rect.x + "; " + first_rect.y);
+  //console.log("Arrow line to  : " + second_rect.x + "; " + second_rect.y);
+
+  //console.log("Right rect side");
+
+  firstPoint = segmentIntersectionBy4Points(
+    second_rect.x + second_rect.width/2, second_rect.y + second_rect.height/2,
+    second_rect.x + second_rect.width/2, second_rect.y - second_rect.height/2,
+    first_rect.x, first_rect.y, second_rect.x, second_rect.y);
+
+  if (firstPoint != undefined) {
+    drawArrow(ctx, first_rect.x + translation.x, first_rect.y + translation.y, firstPoint.x + translation.x, firstPoint.y + translation.y);
+    
+    if (draw_weights) {
+      drawWeightCircleBetweenRects(ctx, first_rect, second_rect, weight, translation);
+    }
+    
+    return;
+  }
+
+  //console.log("Top rect side");
+
+  secondPoint = segmentIntersectionBy4Points(
+    second_rect.x + second_rect.width/2, second_rect.y - second_rect.height/2,
+    second_rect.x - second_rect.width/2, second_rect.y - second_rect.height/2,
+    first_rect.x, first_rect.y, second_rect.x, second_rect.y);
+
+  if (secondPoint != undefined) {
+    drawArrow(ctx, first_rect.x + translation.x, first_rect.y + translation.y, secondPoint.x + translation.x, secondPoint.y + translation.y);
+    
+    if (draw_weights) {
+      drawWeightCircleBetweenRects(ctx, first_rect, second_rect, weight, translation);
+    }
+    
+    return;
+  }
+
+  //console.log("Left rect side");
+
+  thirdPoint = segmentIntersectionBy4Points(
+    second_rect.x - second_rect.width/2, second_rect.y - second_rect.height/2,
+    second_rect.x - second_rect.width/2, second_rect.y + second_rect.height/2,
+    first_rect.x, first_rect.y, second_rect.x, second_rect.y);
+
+  if (thirdPoint != undefined) {
+    drawArrow(ctx, first_rect.x + translation.x, first_rect.y + translation.y, thirdPoint.x + translation.x, thirdPoint.y + translation.y);
+    
+    if (draw_weights) {
+      drawWeightCircleBetweenRects(ctx, first_rect, second_rect, weight, translation);
+    }
+    
+    return;
+  }
+
+  //console.log("Bottom rect side");
+
+  fourthPoint = segmentIntersectionBy4Points(
+    second_rect.x - second_rect.width/2, second_rect.y + second_rect.height/2,
+    second_rect.x + second_rect.width/2, second_rect.y + second_rect.height/2,
+    first_rect.x, first_rect.y, second_rect.x, second_rect.y);
+
+  if (fourthPoint != undefined) {
+    drawArrow(ctx, first_rect.x + translation.x, first_rect.y + translation.y, fourthPoint.x + translation.x, fourthPoint.y + translation.y);
+    
+    if (draw_weights) {
+      drawWeightCircleBetweenRects(ctx, first_rect, second_rect, weight, translation);
+    }
+    
+    return;
+  }
+
+  drawArrow(ctx, first_rect.x + translation.x, first_rect.y + translation.y, second_rect.x + translation.x, second_rect.y + translation.y);
+  
+  if (draw_weights) {
+    drawWeightCircleBetweenRects(ctx, first_rect, second_rect, weight, translation);
+  }
+}
+
+function isPointInsideRect(rect, x, y, translation = {x: 0, y: 0})
+{
+  return (x <= rect.x + translation.x + rect.width  / 2) &&
+    (x >= rect.x + translation.x - rect.width  / 2) &&
+    (y <= rect.y + translation.y + rect.height / 2) &&
+    (y >= rect.y + translation.y - rect.height / 2);
+}
+
---
+++ ui.js
@@ -0,0 +1,474 @@
+/*
+
+Copyright 2018 Anton Jebrak
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+var canvasClientWidth = 0.0;
+var canvasClientHeight = 0.0;
+
+var showWeights = true;
+
+function redraw()
+{
+  // multiply canvas.height and canvas.width by window.devicePixelRatio if you want HDPI display support
+
+  if (Math.abs(canvasClientWidth - canvas.clientWidth) >= 1.0) {
+    canvasClientWidth = canvas.clientWidth;
+    canvas.width = Math.floor(canvasClientWidth);
+  }
+
+  if (Math.abs(canvasClientHeight - canvas.clientHeight) >= 1.0) {
+    canvasClientHeight = canvas.clientHeight;
+    canvas.height = Math.floor(canvasClientHeight);
+  }
+
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = "#EEEEEE";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  for (var i in rects) {
+    var parents = rects[i].parents;
+
+    for (var pKey in parents)
+      drawArrowBetweenRects(ctx, rects[i], rects[pKey], rects[i]["parents"][pKey], showWeights, translation);
+
+    drawRect(ctx, rects[i], i, translation);
+    prev = i;
+  }
+
+  for (var i in rects) {
+    drawRectName(ctx, rects[i], translation);
+  }
+}
+
+function makeCounter()
+{
+  var keys = Object.keys(rects);
+  if (keys.length <= 0)
+    keys.push(0);
+
+  var i = Math.max.apply(Math, keys);
+  return function() { return ++i; }
+}
+
+function getMousePositionOnCanvas(canvas, mouseEvent)
+{
+  var canvasBounds = canvas.getBoundingClientRect();
+  return {
+    x: mouseEvent.clientX - canvasBounds.left,
+    y: mouseEvent.clientY - canvasBounds.top
+  };
+}
+
+function getRectIndexAtCoord(x, y)
+{
+  for (var i in rects) {
+    if (isPointInsideRect(rects[i], x, y, translation)) {
+      return i;
+    }
+  }
+
+  return null;
+}
+
+function showMessage(text)
+{
+  document.getElementById("message").innerHTML = text;
+}
+
+function selectParentsRecursively(rect, deselect=false)
+{
+  for (var i in rect["parents"]) {
+    rects[i]["selected"] = deselect ? 0 : 2;
+    selectParentsRecursively(rects[i], deselect);
+  }
+}
+
+function autoCorrectSliderWeights(changedSlider)
+{
+  //  console.log("=================================")
+  //  console.log("THIS SLIDER NEW VALUE IS: " + changedSlider.value);
+  
+  var sliders = document.getElementsByClassName("weight-slider");
+  var totalValue = 0;
+  var sliderValues = {};
+  var corrections = {};
+  
+  for (var j = 0; j < sliders.length; ++j) {
+    totalValue += parseFloat(sliders[j].value);
+    
+    if (sliders[j] != changedSlider) {
+      sliderValues[sliders[j].getAttribute("id")] = parseFloat(sliders[j].value);
+    }
+  }
+   
+  var sortedSliderKeys = Object.keys(sliderValues).sort(function(a, b) {return (totalValue > 1 ? 1 : -1) * (sliderValues[a] - sliderValues[b])});
+  var totalCorrection = (1 - totalValue);
+  var correction = totalCorrection / (sliders.length - 1);
+  //  console.log("TOTAL VALUE: " + totalValue);
+  //  console.log("TOTAL CORRECTION: " + totalCorrection);
+  //  console.log("CORRECTION PER SLIDER: " + correction);
+
+  for (var k = 0; k < sortedSliderKeys.length; ++k) {
+    var newValue = sliderValues[sortedSliderKeys[k]] + correction;
+    if (newValue < 0.001) {
+      correction += (newValue - 0.001) / (sortedSliderKeys.length - (k + 1));
+      newValue = 0.001;
+      //  console.log("CORRECTION PER SLIDER IS NOW: " + correction);
+    } else if (newValue > 0.999) {
+      correction += (newValue - 0.999) / (sortedSliderKeys.length - (k + 1));
+      newValue = 0.999;
+      //  console.log("CORRECTION PER SLIDER IS NOW: " + correction);
+    }
+    
+    sliderValues[sortedSliderKeys[k]] = newValue;
+  }
+  
+  for (var j = 0; j < sliders.length; ++j) {
+    if (sliders[j] != changedSlider) {
+      sliders[j].value = sliderValues[sliders[j].getAttribute("id")];
+      sliders[j].setAttribute("oldvalue", sliders[j].value);
+    }
+  }
+
+  changedSlider.setAttribute("oldvalue", changedSlider.value);
+}
+
+function selectRect(rectIndex)
+{
+  selectedRectIndex = rectIndex;
+  rects[selectedRectIndex]["selected"] = 1;
+  selectParentsRecursively(rects[selectedRectIndex]);
+  document.getElementById("constrain-weights").checked = false;
+
+  document.getElementById("selected-node-name").value = rects[selectedRectIndex].text;
+  var childrenKeys = [];
+  
+  for (var i in rects) {
+    if (rects[i]["parents"].hasOwnProperty(selectedRectIndex)) {
+      childrenKeys.push(i);
+    }
+  }
+  
+  for (var i = 0; i < childrenKeys.length; ++i) {
+    var child = rects[childrenKeys[i]];
+
+    var sliderRow = document.createElement("div");
+    var slider = document.createElement("input");
+
+    sliderRow.innerHTML = (child.text != "" ? child.text : childrenKeys[i]) + ": ";
+
+    slider.setAttribute("type", "range");
+    slider.setAttribute("min", "0");
+    slider.setAttribute("max", "1");
+    slider.setAttribute("step", "0.001");
+    slider.setAttribute("id", "weight_of_" + childrenKeys[i]);
+    slider.setAttribute("class", "weight-slider");
+    slider.value = child["parents"][selectedRectIndex] == "" ? 0
+      : child["parents"][selectedRectIndex];
+    slider.setAttribute("oldvalue", slider.value);
+
+    slider.onchange = function() {
+      if (document.getElementById("constrain-weights").checked)
+        autoCorrectSliderWeights(this);
+    }
+
+    sliderRow.appendChild(slider);
+    document.getElementById("weight-sliders").appendChild(sliderRow);
+  }
+
+  document.getElementById("selected-node").style.display = "block";
+  redraw();
+}
+
+function deselectRect(do_redraw=false)
+{
+  if (selectedRectIndex === null)
+    return;
+
+  rects[selectedRectIndex]["selected"] = 0;
+  selectParentsRecursively(rects[selectedRectIndex], true);
+  selectedRectIndex = null;
+  document.getElementById("selected-node-name").innerHTML = "";
+  document.getElementById("weight-sliders").innerHTML = "";
+  document.getElementById("selected-node").style.display = "none";
+
+  if (do_redraw) {
+    redraw();
+  }
+}
+
+function cancel()
+{
+  showMessage("");
+  selectLinkParentKey = null;
+  action = Action.DRAG;
+  document.getElementById("link-weight").style.display = "none";
+  document.getElementById("node-name").style.display = "none";
+  document.getElementById("node-name-input").value = "";
+  document.getElementById("cancel").style.display = "none";
+
+  deselectRect();
+  redraw();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+var canvas = document.getElementById("main-canvas");
+
+var rects = {};
+var Action = {
+  DRAG: 0,
+  CREATE_NODE: 1,
+  CREATE_LINK: 3,
+  DELETE_LINK: 4
+};
+
+var action = Action.DRAG;
+var getNextKey  = makeCounter();
+var draggedRect = null;
+var dragCorrectionX = null;
+var dragCorrectionY = null;
+var selectLinkParentKey = null;
+var translating = false;
+var translation = {x: 0, y: 0};
+var prevMouseX = null;
+var prevMouseY = null;
+var willSelectNodeOnMouseUp = false;
+var selectedRectIndex = null;
+
+redraw();
+
+canvas.onmousedown = function(e) {
+  var mouse = getMousePositionOnCanvas(canvas, e);
+  if (e.button == 2) {
+    translating = true;
+    prevMouseX = mouse.x;
+    prevMouseY = mouse.y;
+    return;
+  }
+
+  var clickedRectKey = getRectIndexAtCoord(mouse.x, mouse.y);
+  var clickedRect = rects[clickedRectKey];
+
+  if (action == Action.CREATE_NODE) {
+    if (clickedRect) {
+      return;
+    }
+
+    rects[getNextKey()] = {
+      "x": mouse.x - translation.x,
+      "y": mouse.y - translation.y,
+      "width": 50,
+      "height": 50,
+      "text": document.getElementById("node-name-input").value,
+      "value": 0,
+      "parents": {},
+      "has_children":0
+    };
+
+    cancel();
+    redraw();
+  } else if (action == Action.CREATE_LINK || action == Action.DELETE_LINK) {
+    if (selectLinkParentKey === null) {
+      selectLinkParentKey = clickedRectKey;
+    }
+  } else if (action == Action.DRAG) {
+    if (clickedRect !== null) {
+      willSelectNodeOnMouseUp = true;
+      draggedRect = clickedRect;
+      dragCorrectionX = mouse.x - draggedRect.x;
+      dragCorrectionY = mouse.y - draggedRect.y;
+    }
+  }
+};
+
+canvas.onmouseup = function(e) {
+  translating = false;
+  prevMouseX = null;
+  prevMouseY = null;
+  draggedRect = null;
+  dragCorrectionX = null;
+  dragCorrectionY = null;
+  var mouse = getMousePositionOnCanvas(canvas, e);
+  var i = getRectIndexAtCoord(mouse.x, mouse.y);
+
+  if (action == Action.CREATE_LINK) {
+    if (selectLinkParentKey !== null && selectLinkParentKey != i) {
+      if (!rects[i]["parents"].hasOwnProperty(selectLinkParentKey)) {
+        rects[i]["parents"][selectLinkParentKey] = document.getElementById("link-weight-input").value;
+        rects[selectLinkParentKey]["has_children"] = 1;
+      }
+
+      cancel();
+      redraw();
+    }
+  } else if (action == Action.DELETE_LINK) {
+    if (selectLinkParentKey !== null && selectLinkParentKey != i) {
+      for (var parentKey in rects[i]["parents"])
+      {
+        if (parentKey == selectLinkParentKey) {
+          delete rects[i]["parents"][parentKey];
+          break;
+        }
+      }
+
+      cancel();
+      redraw();
+    }
+  } else if (willSelectNodeOnMouseUp) {
+    if (selectedRectIndex !== null) {
+      deselectRect();
+    }
+
+    selectRect(i);
+    willSelectNodeOnMouseUp = false;
+  }
+};
+
+canvas.onmousemove = function(e) {
+  willSelectNodeOnMouseUp = false;
+
+  if (draggedRect !== null) {
+    var mouse = getMousePositionOnCanvas(canvas, e);
+
+    draggedRect.x = mouse.x - dragCorrectionX;
+    draggedRect.y = mouse.y - dragCorrectionY;
+
+    redraw();
+  } else if (translating) {
+    var mouse = getMousePositionOnCanvas(canvas, e);
+
+    translation.x += mouse.x - prevMouseX;
+    translation.y += mouse.y - prevMouseY;
+
+    prevMouseX = mouse.x;
+    prevMouseY = mouse.y;
+
+    redraw();
+  }
+};
+
+document.getElementById("import").addEventListener('click', function() {
+  var element = document.createElement('div');
+  element.innerHTML = '<input type="file">';
+  var fileInput = element.firstChild;
+
+  fileInput.addEventListener('change', function() {
+    var file = fileInput.files[0];
+
+    if (file.name.match(/\.(txt|json)$/)) {
+      var reader = new FileReader();
+
+      reader.onload = function() {
+        rects = JSON.parse(reader.result);
+        redraw();
+      };
+
+      reader.readAsText(file);
+    } else {
+      alert("File not supported, .txt or .json files only");
+    }
+  });
+
+  fileInput.click();
+});
+
+document.getElementById("export").onclick = function() {
+  if (selectedRectIndex !== null) {
+    deselectRect(true);
+  }
+
+  window.open().document.body.innerHTML += JSON.stringify(rects);
+}
+
+document.getElementById("place-rect").onclick = function() {
+  cancel();
+  action = Action.CREATE_NODE;
+  document.getElementById("node-name").style.display = "block";
+  document.getElementById("cancel").style.display = "block";
+  showMessage("Type in a text label for the new node then click on the canvas to place it.");
+}
+
+document.getElementById("delete-rect").onclick = function() {
+  if (selectedRectIndex === null)
+    return;
+
+  var i = selectedRectIndex;
+  deselectRect();
+
+  for (var j in rects) {
+    for (var parentKey in rects[j]["parents"]) {
+      if (parentKey == i) {
+        delete rects[j]["parents"][parentKey];
+      }
+    }
+  }
+
+  delete rects[i];
+  redraw();
+}
+
+document.getElementById("create-link").onclick = function() {
+  cancel();
+  action = Action.CREATE_LINK;
+  showMessage("Enter edge weight, select a parent node, then select a child to create an edge");
+  document.getElementById("link-weight").style.display = "block";
+  document.getElementById("cancel").style.display = "block";
+}
+
+document.getElementById("delete-link").onclick = function() {
+  cancel();
+  action = Action.DELETE_LINK;
+  showMessage("Select a parent node, then a child to delete an edge");
+  document.getElementById("cancel").style.display = "block";
+}
+
+document.getElementById("show-weights").onchange = function() {
+  showWeights = document.getElementById("show-weights").checked;
+  redraw();
+}
+
+document.getElementById("save-node").onclick = function() {
+  rects[selectedRectIndex].text = document.getElementById("selected-node-name").value;
+
+  var sliders = document.getElementsByClassName("weight-slider");
+
+  for (var i = 0; i < sliders.length; ++i) {
+    var childId = sliders[i].getAttribute("id").replace("weight_of_", "");
+    //console.log("CHILDID: " + sliders[i].getAttribute("id"));
+    rects[childId]["parents"][selectedRectIndex] = sliders[i].value;
+  }
+
+  redraw();
+}
+
+document.getElementById("reset-node").onclick = function() {
+  document.getElementById("selected-node-name").value = rects[selectedRectIndex].text;
+
+  var sliders = document.getElementsByClassName("weight-slider");
+
+  for (var i = 0; i < sliders.length; ++i) {
+    var childId = sliders[i].getAttribute("id").replace("weight_of_", "");
+    sliders[i].value = rects[childId]["parents"][selectedRectIndex];
+  }
+}
+
+document.getElementById("cancel").onclick = function() {
+  cancel();
+}
+
